fb26484b2422b3d0b85fa7d48cedcb5eb93fa73b
paweltar <paweltar@gmail.com>
2017-01-19 11:17:19 +0100

    After lesson 7

---
+++ content.php
@@ -0,0 +1,5 @@
+<h3><?php the_title(); ?></h3>
+<div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
+<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('G:i'); ?>, in <?php the_category(); ?></small>
+<p><?php the_content(); ?></p>
+<hr>
--- header.php
+++ header.php
@@ -17,3 +17,5 @@
 
     <?php wp_nav_menu(array('theme_location' => 'primary' )); ?>
     <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+    <hr>
+    <br>
